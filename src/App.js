bf2aea06e748f38a7e77ea4211ac0e6368ef1734
Andrew <andrewmoren93@gmail.com>
2022-11-03 07:24:36 +0300

    add search

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [cartOpened, setCartOpened] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     fetch("https://63452c19dcae733e8feb2bd7.mockapi.io/sneakers")
@@ -22,6 +23,10 @@ function App() {
     setCartItems((prev) => [...prev, obj]);
   };
 
+  const onSearchInput = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className="wrapper clear">
       {cartOpened && (
@@ -33,19 +38,33 @@ function App() {
           <h1>All shoes</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Search...." />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className=" clear cu-p"
+                src="/img/btn-remove.svg"
+                alt="Clear"
+              />
+            )}
+            <input
+              value={searchValue}
+              onChange={onSearchInput}
+              placeholder="Search...."
+            />
           </div>
         </div>
 
         <div className="d-flex flex-wrap">
-          {items.map((item, id) => (
-            <Card
-              {...item}
-              key={id}
-              onClickFavorite={() => console.log("Add to favorite")}
-              onPlus={(obj) => onAddToCard(obj)}
-            />
-          ))}
+          {items
+            .filter((item) => item.name.toLowerCase().includes(searchValue))
+            .map((item, id) => (
+              <Card
+                {...item}
+                key={id}
+                onClickFavorite={() => console.log("Add to favorite")}
+                onPlus={(obj) => onAddToCard(obj)}
+              />
+            ))}
         </div>
       </div>
     </div>
